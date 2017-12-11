7b3b8cbdcabf66fc1279e9a167af7b0a9af52641
Zhiming Shen <zshen@cs.cornell.edu>
2017-12-11 15:36:19 -0500

    add some comments

--- linux-kernel/get-linux-3.4.46.sh
+++ linux-kernel/get-linux-3.4.46.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+#this is for downloading a clean version of linux 3.4.46 that can work with the kvm and hyper-v drivers
 WEBSITE="http://fireless.cs.cornell.edu/supercloud/downloads"
 wget $WEBSITE/linux-3.4.46-8.el6.x86_64.tgz
